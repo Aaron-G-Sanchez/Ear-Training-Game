7bc24fdd9ead0a70bec27e8e2eb826c25b1649c8
Aaron Sanchez <saarong12@gmail.com>
2022-10-03 11:43:29 -0500

    Sample play pause buttons respond to click event

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-console.log('BING CHILLING')
+const AudioContext = window.AudioContext || window.webkitAudioContext
+
+const audioContext = new AudioContext()
+
+// Game variables
 
 const frequencies = [250, 440, 500, 1000, 2000]
 
@@ -8,18 +12,24 @@ const question = document.querySelector('h1')
 
 const answer = '1000Hz'
 
-// Come back to -- basic quiz logic
-// const quizFunction = () => {
-//   if (button[i].innerHTML == answer) {
-//     console.log('You are right')
-//   }
-// }
+// play pause buttons
+const play = document.querySelector('#play')
+const pause = document.querySelector('#pause')
+
+play.addEventListener('click', () => {
+  console.log('Play')
+})
+
+pause.addEventListener('click', () => {
+  console.log('Pause')
+})
 
 // Displays the frequencys from the array of frequencies on the buttons
 for (let i = 0; i < button.length; i++) {
   button[i].innerHTML = `${frequencies[i]}Hz`
 }
 
+// Loops through the buttons and looks at the innerHTML
 for (let i = 0; i < button.length; i++)
   button[i].addEventListener('click', () => {
     if (button[i].innerHTML == answer) {
