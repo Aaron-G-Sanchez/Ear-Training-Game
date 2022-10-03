04a22d3d9736fb014a3421ac93a2941f3470a534
Aaron Sanchez <saarong12@gmail.com>
2022-10-03 11:23:09 -0500

    Buttons have event listeners

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const frequencies = [250, 440, 500, 1000, 2000]
 
 const button = document.querySelectorAll('button')
 
-const testHTML = document.querySelector('h1')
+const question = document.querySelector('h1')
 
-testHTML.innerHTML = 'BING CHILLING'
+const answer = 1000
 
+// Come back to -- basic quiz logic
+// const quizFunction = () => {
+//   if (button[i].innerHTML == answer) {
+//     console.log('You are right')
+//   }
+// }
+
+// Displays the frequencys from the array of frequencies on the buttons
 for (let i = 0; i < button.length; i++) {
-  button[i].innerHTML = frequencies[i]
+  button[i].addEventListener('click', () => {
+    console.log('clicked')
+  })
+  button[i].innerHTML = `${frequencies[i]} Hz`
 }
