0b6cacad1b4cb6cda95875c9995d8697d367fbb0
Aaron Sanchez <saarong12@gmail.com>
2022-10-06 21:46:18 -0500

    Both game modes have complete levels

--- altGameMode.js
+++ altGameMode.js
@@ -8,33 +8,6 @@ let timerOnStart = 5
 
 let unmuteTimer = 03
 
-// let frequencies = {
-//   C3: 130.813,
-//   Cs3: 138.591,
-//   D3: 146.832,
-//   Ds3: 155.563,
-//   E3: 164.814,
-//   F3: 174.614,
-//   Fs3: 184.997,
-//   G3: 195.998,
-//   Gs3: 207.652,
-//   A3: 220,
-//   As3: 233.082,
-//   B3: 246.942,
-//   C4: 261.626,
-//   Cs4: 277.183,
-//   D4: 293.665,
-//   Ds4: 311.127,
-//   E4: 329.628,
-//   F4: 349.228,
-//   Fs4: 369.994,
-//   G4: 391.995,
-//   Gs4: 415.305,
-//   A4: 440,
-//   As4: 466.164,
-//   B4: 493.883
-// }
-
 let octaveOne = [
   130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998,
   207.652, 220, 233.082, 246.942
@@ -45,7 +18,13 @@ let octaveTwo = [
   415.305, 440, 466.164, 493.883
 ]
 
-let bothOctaves = [...octaveOne, ...octaveTwo]
+// let bothOctaves = [...octaveOne, ...octaveTwo]
+
+let bothOctaves = [
+  130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998,
+  207.652, 220, 233.082, 246.942, 261.626, 277.183, 293.665, 311.127, 329.628,
+  349.228, 369.994, 391.995, 415.305, 440, 466.164, 493.883
+]
 
 // const pitch = Object.keys(frequencies)
 // const freq = Object.values(frequencies)
@@ -68,12 +47,7 @@ timer.innerHTML = `Press Start`
 
 level.innerHTML = 'LEVEL 1'
 
-// Picks at random a frequency from the object
-// let pitchGenerator = () => {
-//   let randomPitch = freq[Math.floor(Math.random() * freq.length)]
-//   return randomPitch
-// }
-
+// Picks at random a frequency from the arrays
 let octaveOneFreqGen = () => {
   let randomNumber = Math.floor(Math.random() * octaveOne.length)
   return randomNumber
@@ -88,12 +62,20 @@ let bothOctaveFreqGen = () => {
   let randomNumber = Math.floor(Math.random() * bothOctaves.length)
   return randomNumber
 }
-// Selects random value from object
-// let answer = pitchGenerator()
-
+// Selects random index from array
 let answer = octaveOne[octaveOneFreqGen()]
 console.log(answer)
-// let answer = 174.614
+
+// Audio mute
+const unmute = () => {
+  let unmuteInterval = setInterval(() => {
+    unmuteTimer--
+    if (unmuteTimer === 0) {
+      clearInterval(unmuteInterval)
+      gain.gain.value = 0.03
+    }
+  }, 1000)
+}
 
 // Oscillator and gain node
 let osc = audioContext.createOscillator()
@@ -143,28 +125,61 @@ mute.addEventListener('click', () => {
 
 // Game Functions
 const hideSecondOctave = () => {
-  for (let i = 0; i < buttons.length; i++) {
+  for (let i = 0; i < secondOctave.length; i++) {
     secondOctave[i].style.opacity = 0
   }
 }
 
 const hideFirstOctave = () => {
-  for (let i = 0; i < buttons.length; i++) {
+  for (let i = 0; i < firstOctave.length; i++) {
     firstOctave[i].style.opacity = 0
   }
 }
 const showSecondOctave = () => {
-  for (let i = 0; i < buttons.length; i++) {
+  for (let i = 0; i < secondOctave.length; i++) {
     secondOctave[i].style.opacity = 1
   }
 }
 
 const showFirstOctave = () => {
-  for (let i = 0; i < buttons.length; i++) {
+  for (let i = 0; i < firstOctave.length; i++) {
     firstOctave[i].style.opacity = 1
   }
 }
 
+// Sets up the game space and levels
+hideSecondOctave()
+
+const levelTwo = () => {
+  level.innerHTML = 'LEVEL 2'
+  console.log('im level 2')
+
+  updateAnswer()
+}
+
+const levelThree = () => {
+  level.innerHTML = 'LEVEL 3'
+  console.log('im level 3')
+  hideFirstOctave()
+  showSecondOctave()
+
+  levelThreeUpdateAnswer()
+}
+
+const levelFour = () => {
+  level.innerHTML = 'LEVEL 4'
+  console.log('im level 4')
+
+  levelThreeUpdateAnswer()
+}
+
+const levelFive = () => {
+  level.innerHTML = 'LEVEL 5'
+  console.log('im level 5')
+  showFirstOctave()
+
+  levelFiveUpdateAnswer()
+}
 // Updates answer as well as OSC frequency
 let updateAnswer = () => {
   octaveOneFreqGen()
@@ -172,6 +187,34 @@ let updateAnswer = () => {
   console.log(`NEW ANSWER:${answer}`) //logs new answer
 
   oscFreq.setValueAtTime(answer, audioContext.currentTime)
+
+  // After Delay plays audio again
+  unmuteTimer = 03
+  unmute()
+}
+
+let levelThreeUpdateAnswer = () => {
+  octaveTwoFreqGen()
+  answer = octaveTwo[octaveTwoFreqGen()]
+  console.log(`NEW ANSWER:${answer}`) //logs new answer
+
+  oscFreq.setValueAtTime(answer, audioContext.currentTime)
+
+  // After delay plays audio again
+  unmuteTimer = 03
+  unmute()
+}
+
+let levelFiveUpdateAnswer = () => {
+  bothOctaveFreqGen()
+  answer = bothOctaves[bothOctaveFreqGen()]
+  console.log(`NEW ANSWER:${answer}`) //logs new answer
+
+  oscFreq.setValueAtTime(answer, audioContext.currentTime)
+
+  // After delay plays audio again
+  unmuteTimer = 03
+  unmute()
 }
 
 // Loops through the buttons looking for the correct answer
@@ -182,7 +225,26 @@ let buttonClicks = () => {
       console.log(buttonFreq)
       if (buttonFreq == answer) {
         console.log('YOU ARE CORRECT')
-        updateAnswer()
+        gain.gain.value = 0
+        if (turnCounter <= 11) {
+          updateAnswer()
+        } else if (turnCounter >= 12 && turnCounter <= 25) {
+          levelThreeUpdateAnswer()
+        } else if (turnCounter >= 26) {
+          levelFiveUpdateAnswer()
+        }
+        turnCounter++
+
+        // Turn counter rules
+        if (turnCounter === 6) {
+          levelTwo()
+        } else if (turnCounter === 12) {
+          levelThree()
+        } else if (turnCounter === 18) {
+          levelFour()
+        } else if (turnCounter === 25) {
+          levelFive()
+        }
       }
     })
   }
